bb1d7aff58de3022f060985ed918b05fc13d0d10
Harel Panker <harelpanker@gmail.com>
2025-03-31 19:03:49 +0200

    Orgenized layout

---
+++ lib/fonts.ts
@@ -0,0 +1,11 @@
+import { Poppins, IBM_Plex_Mono } from 'next/font/google';
+
+export const poppins = Poppins({
+	weight: ['400', '500', '600', '700'],
+	subsets: ['latin'],
+});
+
+export const ibmPlexMono = IBM_Plex_Mono({
+	weight: ['400', '500', '600', '700'],
+	subsets: ['latin'],
+});
---
+++ lib/metadata.ts
@@ -0,0 +1,6 @@
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+	title: 'Permit.io CLI Launch Week',
+	description: 'Developer-First Integrated Authorization',
+};
