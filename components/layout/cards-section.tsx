5d92a01f3a271bda95869ad7573a3493bf705b12
Harel Panker <harelpanker@gmail.com>
2025-04-02 16:09:15 +0200

    fix build error

--- components/layout/cards-section.tsx
+++ components/layout/cards-section.tsx
@@ -25,6 +25,8 @@ export default function CardsSection({
 			<Section className='flex flex-col gap-y-14 !px-0 pt-20 lg:gap-y-24 lg:!px-12 lg:pb-36 lg:pt-24'>
 				<Container className='flex flex-col gap-y-14 px-4 lg:gap-y-20 xl:px-0'>
 					<Header title={title} subtitle={subtitle} />
+					<HomeCards />
+					<SubscribeCards />
 					{isHomePage ? <HomeCards /> : null}
 					{isSubscribePage ? <SubscribeCards /> : null}
 				</Container>
