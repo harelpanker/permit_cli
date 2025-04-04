60315c991b07375e7c1acae6dfedafff76ba01ca
Harel Panker <harelpanker@gmail.com>
2025-04-04 13:52:28 +0200

    Adding dynamic name

--- app/subscribe/page.tsx
+++ app/subscribe/page.tsx
@@ -1,13 +1,19 @@
 import HeroSection from '@/components/layout/hero-section';
 import MobileNavbar from '@/components/layout/mobile-navbar';
 import CardsSection from '@/components/layout/cards-section';
+import { getUser } from '@/lib/auth';
 
-export default function SubscribePage() {
+export default async function SubscribePage() {
+	const user = await getUser();
+	const name = user?.user_metadata?.full_name;
+
+	// console.log(user?.user_metadata); // avatar_url && full_name
+
 	return (
 		<>
 			<MobileNavbar ctaIcon={false} ctaText='Sign Up for Livestreams' href='https://lu.ma/plkwvl5v' />
 			<main>
-				<HeroSection showBottom={false} name='Gabriel Manor' number={304} />
+				<HeroSection userName={name || 'Gabriel Manor'} showBottom={false} />
 				<CardsSection
 					isSubscribePage={true}
 					isHomePage={false}
