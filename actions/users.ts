aa7d6ab291920b45eabb91c7a03205b77af0e935
Harel Panker <harelpanker@gmail.com>
2025-04-04 15:47:15 +0200

    change to static url

--- actions/users.ts
+++ actions/users.ts
@@ -2,38 +2,22 @@
 
 import { getSupabaseAuth } from '@/lib/auth';
 import { Provider } from '@supabase/supabase-js';
-import { headers } from 'next/headers';
 
 export const loginAction = async (provider: Provider) => {
 	try {
-		const headersList = headers();
-		const host = (await headersList).get('host');
-
-		if (!host) {
-			console.error('Login Action Error: Host header is missing.');
-			return { errorMessage: 'Configuration error: Cannot determine application host.' };
-		}
-		const protocol = process.env.NODE_ENV === 'development' ? 'http' : 'https';
-
-		const baseUrl = `${protocol}://${host}`;
-		const redirectTo = `${baseUrl}/api/auth/`;
-
-		const supabase = await getSupabaseAuth();
-		const { data, error } = await supabase.signInWithOAuth({
+		const { data, error } = await (
+			await getSupabaseAuth()
+		).signInWithOAuth({
 			provider,
 			options: {
-				redirectTo: redirectTo,
+				redirectTo: `https://deploy-preview-1--permicli.netlify.app/api/auth/`,
 			},
 		});
 
-		if (error) {
-			console.error('Supabase OAuth Error:', error.message);
-			throw error;
-		}
+		if (error) throw error;
 
 		return { errorMessage: null, url: data.url };
 	} catch (error) {
-		console.error('Login Action Error:', error instanceof Error ? error.message : error);
-		return { errorMessage: 'Error logging in. Please try again.' };
+		return { errorMessage: 'Error login in' };
 	}
 };
