75df1f7af246b25f7d1882cdcddb13fb30fb0f7f
Harel Panker <harelpanker@gmail.com>
2025-04-05 14:48:59 +0200

    small build fix

--- app/api/auth/route.ts
+++ app/api/auth/route.ts
@@ -6,7 +6,8 @@ export async function GET(request: Request) {
 	const { searchParams, origin } = new URL(request.url);
 	const code = searchParams.get('code');
 	// if "next" is in param, use it as the redirect URL
-	const next = searchParams.get('next') ?? '/subscribe';
+	// const next = searchParams.get('next') ?? '/subscribe';
+	const next = '/subscribe';
 
 	if (code) {
 		const cookieStore = cookies();
